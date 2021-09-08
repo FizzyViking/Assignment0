86ce73d2ad284d4af05be1b9856f800f8b954bd0
alexander.rode2 <maler2@live.dk>
2021-09-08 18:54:52 +0200

    Tested and Implemented IsLeapYear

--- Console.Tests/UnitTest1.cs
+++ Console.Tests/UnitTest1.cs
@@ -6,9 +6,22 @@ namespace Console.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsLeapYear()
         {
+            // Arrange
+            Program program = new Program();
 
+            // Act
+            bool result1 = program.IsLeapYear(440);
+            bool result2 = program.IsLeapYear(53);
+            bool result3 = program.IsLeapYear(1200);
+            bool result4 = program.IsLeapYear(1700);
+
+            // Assert
+            Assert.Equal(true, result1);
+            Assert.Equal(false, result2);
+            Assert.Equal(true, result3);
+            Assert.Equal(false, result4);
         }
     }
 }
