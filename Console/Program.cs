5529816fe29cbe8376306eb55735c542e47c188a
alexander.rode2 <maler2@live.dk>
2021-09-09 14:04:27 +0200

    Created user interface for IsLeapYear

--- Console/Program.cs
+++ Console/Program.cs
@@ -6,7 +6,17 @@ namespace Console
     {
         public static void Main(string[] args)
         {
-            
+            Program program = new Program();
+            program.UserInput();
+        }
+
+        public void UserInput() {
+            System.Console.WriteLine("Enter a year: ");
+            int input = int.Parse(System.Console.ReadLine());
+
+            if (IsLeapYear(input)) {
+                System.Console.WriteLine("yay");
+            } else System.Console.WriteLine("nay");
         }
 
         public bool IsLeapYear(int year) {
